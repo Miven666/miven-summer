aa1ed6d5d8d8c9c63054eaa2d831e6017faa67c4
mingzhi.xie <xie6032mail@163.com>
2019-07-18 15:51:19 +0800

    Update core entity module

---
+++ miven-entity/src/main/java/com/miven/entity/Student.java
@@ -0,0 +1,18 @@
+package com.miven.entity;
+
+import lombok.Builder;
+
+/**
+ * 学生
+ *
+ * @author mingzhi.xie
+ * @date 2019/5/29.
+ */
+
+@Builder
+public class Student {
+
+    private long id;
+
+    private String name;
+}
