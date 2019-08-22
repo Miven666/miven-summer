e4697dcfc3937ffc2f062a50a625717e28f36347
mingzhi.xie <xie6032mail@163.com>
2019-08-22 16:54:15 +0800

    Add Fruit

---
+++ miven-entity/src/main/java/com/miven/entity/Fruit.java
@@ -0,0 +1,25 @@
+package com.miven.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 水果
+ * @author mingzhi.xie
+ * @date 2019/3/20
+ */
+
+@Data
+public class Fruit implements Serializable {
+
+    private static final long serialVersionUID = 1047204607917402451L;
+
+    private int id;
+
+    private String name;
+
+    private int number;
+
+
+}
