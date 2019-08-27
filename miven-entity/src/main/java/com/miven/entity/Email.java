51b0973b7ef9aa75b87c83a6a10c62d5c5a737b7
mingzhi.xie <xie6032mail@163.com>
2019-08-27 14:51:04 +0800

    Add @AllArgsConstructor

--- miven-entity/src/main/java/com/miven/entity/Email.java
+++ miven-entity/src/main/java/com/miven/entity/Email.java
@@ -1,5 +1,6 @@
 package com.miven.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -9,6 +10,7 @@ import lombok.Data;
  * @since 1.0
  */
 @Data
+@AllArgsConstructor
 public class Email {
 
     private String to;
