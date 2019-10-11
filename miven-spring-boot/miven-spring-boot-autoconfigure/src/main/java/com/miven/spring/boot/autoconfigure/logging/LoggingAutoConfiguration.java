3030b73bfba9cde40326b5c9b9211f9a7be22462
mingzhi.xie <xie6032mail@163.com>
2019-10-11 15:44:11 +0800

    Add miven spring boot autoconfigure

---
+++ miven-spring-boot/miven-spring-boot-autoconfigure/src/main/java/com/miven/spring/boot/autoconfigure/logging/LoggingAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.miven.spring.boot.autoconfigure.logging;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 日志自动配置类
+ * @author mingzhi.xie
+ * @date 2019/10/11
+ * @since 1.0
+ */
+@Configuration
+public class LoggingAutoConfiguration {
+}
